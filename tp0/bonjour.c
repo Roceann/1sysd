2dbfea578f36ade93b1c72f411eeb77c0379d230
raph <raph@exemple.com>
2023-12-19 10:30:49 +0100

    TP0 terminé

---
+++ tp0/bonjour.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/* Ceci est mon premier
+   programme écrit en langage C
+*/
+
+// Fonction main
+// coucou
+
+int main() {
+    printf("Bonjour à tous !\n");
+    exit(0);
+}
+
